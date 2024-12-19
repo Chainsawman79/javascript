6f42ab4f2c54cbbf3469536d5798124e7943c5cd
Charchit73 <148005866+Chainsawman79@users.noreply.github.com>
2024-12-19 16:29:11 +0530

    Java Script Practice 19/12/24

---
+++ script.js
@@ -0,0 +1,82 @@
+// function myFunction()
+// {
+//     alert("Hello World!");
+// }
+// function sayHello(name,age){
+//     document.write(name+" <h1>is</h1>"+age +" <h1>years old</h1>");
+// }
+// function add(a,b)
+// {
+//     console.log(a+b);
+//     document.write(a+b);
+
+// }
+// add(10,20);
+// var res=mul(12,30);
+// function mul(x,y)
+// {
+//     return x*y;
+// }
+// document.write(res);
+// const square=function(number){
+//     return number*number;
+// };
+// const x=square(4);
+// console.log(x);
+// function myFunction(g1,g2)
+// {
+//     return g1/g2;
+// }
+// const value=myFunction(8,2);
+// console.log(value);
+// function add(a,b)
+// {
+//     console.log(a+b);
+//     document.write(a+b);
+
+// }
+// add(10,20);
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// };
+// add2(102,20);
+// //arrow function
+// var add3=(a,b)=>{
+//     console.log(a+b);
+// };
+// add3(30,20);
+// var a=100;
+// var b=13;
+// var c=10;
+// var linebreak='<br>';
+// document.write("a+b-c=");
+// result=a+b-c;
+// document.write(result);
+// document.write(linebreak);
+// document.write("a/b=");
+// result=a/b;
+// document.write(result);
+// document.write(linebreak);
+// function myFunction()
+// {
+//     var x=5+5;
+//     var y ="5"+5;
+//     var z="Hello"+5;
+//     var demoP=document.getElementById("demo");
+//     demoP.innerHTML=x+"<br>"+y+"<br>"+z;
+// }
+// x=10,y=3;
+// if(x==10 && y>1)
+// {
+
+// }
+// else if(x==6 || y==5)
+// {
+
+// }
+// if (!(x==y))
+// {
+//     document.write("Hello World!");
+// }
+var myCollection=[1,"meera mam",true,{name:"sanjeev",age:30},"abhishek","bachhan","Amitabh"];
+
